1a6148088eb08d0c29dff0082771264a20183783
Ivan <ivanzhangofficial@gmail.com>
2022-01-25 16:46:29 -0500

    add theme

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import {extendTheme} from 'native-base';
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      50: '#fae6ff',
+      100: '#e6b9fb',
+      200: '#d78cf4',
+      300: '#cb5eee',
+      400: '#af30e8',
+      500: '#8918cf',
+      600: '#6111a1',
+      700: '#3e0b74',
+      800: '#210547',
+      900: '#0a001c',
+    },
+  },
+  config: {},
+});
+
+export default theme;
