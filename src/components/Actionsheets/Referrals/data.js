ac5801887f56d103a1cbf154c0c541515764e99d
Ivan <ivanzhangofficial@gmail.com>
2022-01-26 22:05:24 -0500

    add data for referrals sheet

---
+++ src/components/Actionsheets/Referrals/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {id: 1, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 2, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 3, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 4, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 5, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 6, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 7, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 8, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 9, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 10, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 11, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 12, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 13, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 14, name: 'Max Verstappen', date: 'April 20, 2022'},
+  {id: 15, name: 'Max Verstappen', date: 'April 20, 2022'},
+];
+
+export default data;
