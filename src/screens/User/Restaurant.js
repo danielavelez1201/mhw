0ef01a079f803d5685442b002c244315af303fa4
Ivan <ivanzhangofficial@gmail.com>
2022-01-27 13:28:01 -0500

    add restaurant screen to nav

--- src/screens/User/Restaurant.js
+++ src/screens/User/Restaurant.js
@@ -1,7 +1,8 @@
 import {Image, ScrollView, Box, Text, Container} from 'native-base';
-import React from 'react';
+import React, {useState} from 'react';
+import {TouchableOpacity} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import AdaptiveSafeAreaView from '../../components/AdaptiveSafeAreaView';
+import Leaderboard from '../../components/Actionsheets/Leaderboard';
 import Carousel from '../../components/Carousel';
 import cardShadow from '../../constants/cardShadow';
 import primaryCardShadow from '../../constants/primaryCardShadow';
@@ -11,8 +12,9 @@ import theme from '../../theme';
 import people from './people';
 
 function Restaurant(props) {
+  const [leaderboardSheetOpen, toggleLeaderboardSheet] = useState(false);
   return (
-    <AdaptiveSafeAreaView style={{backgroundColor: 'white'}}>
+    <Box flex={1} backgroundColor="white">
       <Carousel />
       <Box
         style={{
@@ -57,55 +59,61 @@ function Restaurant(props) {
           marginBottom={50}
           borderRadius={10}
           style={primaryCardShadow}>
-          <Box
-            flexDirection="row"
-            alignItems="center"
-            marginHorizontal={10}
-            style={{width: '100%'}}>
-            <Image
-              source={{
-                uri: 'https://i.picsum.photos/id/326/200/200.jpg?hmac=T_9V3kc7xrK46bj8WndwDhPuvpbjnAM3wfL_I7Gu6yA',
-              }}
-              resizeMode="contain"
-              alt="NFT"
-              borderRadius={100}
-              style={{width: 120, height: 120, marginRight: 10}}
-            />
-            <Box style={{width: '100%'}}>
-              <Text flex={1} fontSize={24} fontWeight="bold" color={'#FFD500'}>
-                Star Patron
-              </Text>
-              <Container h="2" mb="1">
-                <LinearGradient
-                  start={{x: 0, y: 0}}
-                  end={{x: 1, y: 0}}
-                  colors={['#4BE1EC', '#8AA1ED', '#CB5EEE'].slice(
-                    0,
-                    Math.round(3 * (270 / 1000)) + 1,
-                  )} // all three colors if full, only 1 or 2 depending on progress
-                  style={{
-                    zIndex: 1,
-                    paddingRight: 120 * (270 / 1000), // from 0 to 120 based on progress
-                    borderRadius: 5,
-                  }}>
-                  <Text></Text>
-                </LinearGradient>
-                <Container
-                  zIndex="-1"
-                  w="160"
-                  h="2"
-                  bg="white"
-                  borderWidth={1}
-                  borderRadius={10}
-                  borderColor="#DADADA"
-                  position="absolute"></Container>
-              </Container>
-              <Text fontSize="md" fontWeight="bold" color="black">
-                <Text color={theme.colors.primary[300]}>270</Text> / 275 to{' '}
-                <Text color="#DE9E11">Legendary</Text>
-              </Text>
+          <TouchableOpacity onPress={() => toggleLeaderboardSheet(true)}>
+            <Box
+              flexDirection="row"
+              alignItems="center"
+              marginHorizontal={10}
+              style={{width: '100%'}}>
+              <Image
+                source={{
+                  uri: 'https://i.picsum.photos/id/326/200/200.jpg?hmac=T_9V3kc7xrK46bj8WndwDhPuvpbjnAM3wfL_I7Gu6yA',
+                }}
+                resizeMode="contain"
+                alt="NFT"
+                borderRadius={100}
+                style={{width: 120, height: 120, marginRight: 10}}
+              />
+              <Box style={{width: '100%'}}>
+                <Text
+                  flex={1}
+                  fontSize={24}
+                  fontWeight="bold"
+                  color={'#FFD500'}>
+                  Star Patron
+                </Text>
+                <Container h="2" mb="1">
+                  <LinearGradient
+                    start={{x: 0, y: 0}}
+                    end={{x: 1, y: 0}}
+                    colors={['#4BE1EC', '#8AA1ED', '#CB5EEE'].slice(
+                      0,
+                      Math.round(3 * (270 / 1000)) + 1,
+                    )} // all three colors if full, only 1 or 2 depending on progress
+                    style={{
+                      zIndex: 1,
+                      paddingRight: 120 * (270 / 1000), // from 0 to 120 based on progress
+                      borderRadius: 5,
+                    }}>
+                    <Text></Text>
+                  </LinearGradient>
+                  <Container
+                    zIndex="-1"
+                    w="160"
+                    h="2"
+                    bg="white"
+                    borderWidth={1}
+                    borderRadius={10}
+                    borderColor="#DADADA"
+                    position="absolute"></Container>
+                </Container>
+                <Text fontSize="md" fontWeight="bold" color="black">
+                  <Text color={theme.colors.primary[300]}>270</Text> / 275 to{' '}
+                  <Text color="#DE9E11">Legendary</Text>
+                </Text>
+              </Box>
             </Box>
-          </Box>
+          </TouchableOpacity>
         </Box>
         <Box
           backgroundColor="white"
@@ -115,33 +123,40 @@ function Restaurant(props) {
           marginBottom={50}
           borderRadius={10}
           style={cardShadow}>
-          <Box
-            flexDirection="row"
-            alignItems="center"
-            marginHorizontal={10}
-            style={{width: '100%'}}>
-            <Image
-              source={LeaderboardIcon}
-              resizeMode="contain"
-              alt="Leaderboard"
-              borderRadius={100}
-              style={{width: 120, height: 120, marginRight: 10}}
-            />
-            <Box style={{width: '100%'}}>
-              <Text
-                fontSize={24}
-                fontWeight="bold"
-                color={theme.colors.primary[300]}>
-                Leaderboard
-              </Text>
-              <Text fontSize={16} fontWeight="bold" color="#979797">
-                <Text color={theme.colors.primary[300]}>#1</Text> of 1000
-              </Text>
+          <TouchableOpacity onPress={() => toggleLeaderboardSheet(true)}>
+            <Box
+              flexDirection="row"
+              alignItems="center"
+              marginHorizontal={10}
+              style={{width: '100%'}}>
+              <Image
+                source={LeaderboardIcon}
+                resizeMode="contain"
+                alt="Leaderboard"
+                borderRadius={100}
+                style={{width: 120, height: 120, marginRight: 10}}
+              />
+              <Box style={{width: '100%'}}>
+                <Text
+                  fontSize={24}
+                  fontWeight="bold"
+                  color={theme.colors.primary[300]}>
+                  Leaderboard
+                </Text>
+                <Text fontSize={16} fontWeight="bold" color="#979797">
+                  <Text color={theme.colors.primary[300]}>#1</Text> of 1000
+                </Text>
+              </Box>
             </Box>
-          </Box>
+          </TouchableOpacity>
         </Box>
       </Box>
-    </AdaptiveSafeAreaView>
+      <Leaderboard
+        onClose={() => {}}
+        isVisible={leaderboardSheetOpen}
+        toggleVisible={toggleLeaderboardSheet}
+      />
+    </Box>
   );
 }
 
